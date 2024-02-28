4a4c4e979afba9e98bcff7cc957082adc5ae98c8
QalibH <qalib.wot@gmail.com>
2024-02-28 15:47:57 +0400

    first commit

---
+++ hiu.py
@@ -0,0 +1,8 @@
+user_data = int(input("enter the number: "))
+
+if user_data > 5:
+    print ("your nubmer bigger than 5")
+if user_data == 5:
+    print ("your nubmer equal to 5")
+if user_data < 5:
+    print ("your nubmer less than 5")
