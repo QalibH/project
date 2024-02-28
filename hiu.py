de369840ab14fa86bce7367e92dd4a6a6fdf644e
QalibH <qalib.wot@gmail.com>
2024-02-28 15:56:20 +0400

    second

--- hiu.py
+++ hiu.py
@@ -1,8 +1,10 @@
 user_data = int(input("enter the number: "))
 
-if user_data > 5:
-    print ("your nubmer bigger than 5")
-if user_data == 5:
-    print ("your nubmer equal to 5")
-if user_data < 5:
-    print ("your nubmer less than 5")
+if user_data > 7:
+    print ("your nubmer bigger than 7")
+if user_data == 7:
+    print ("your nubmer equal to 7")
+if user_data < 7:
+    print ("your nubmer less than 7")
+
+print ("hello")
